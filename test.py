b38b8284cf81ecb541be1b1f792c05630c6e6022
Jasmehar Kaur <jasmehar.kr@gmail.com>
2026-02-07 11:50:21 -0500

    refactor

--- test.py
+++
@@ -1,9 +0,0 @@
-from move_cursor import move_cursor, left_click, right_click
-import time
-
-move_cursor(500, 500)
-time.sleep(1)
-# move_cursor(1000, 1000)
-time.sleep(1)
-left_click()
-right_click()
